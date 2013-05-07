8e591e9ec7da236e318f898dd26d659cc472d78f
Thanh Dang Hoang Minh <InkyAshizuki@gmail.com>
2013-05-07 15:12:20 +0200

    Set MySQL5InnoDBDialect Match = reserved keyword in MySQL, changed table name to Game

--- src/main/java/cz/muni/fi/pv243/sportleaguesystem/entities/Match.java
+++ src/main/java/cz/muni/fi/pv243/sportleaguesystem/entities/Match.java
@@ -9,12 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
-import javax.validation.constraints.Future;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import javax.validation.constraints.*;
 
 
@@ -23,6 +21,7 @@ import javax.validation.constraints.*;
 * @author Filip Bogyai
 */
 @Entity
+@Table(name="Game")
 public class Match {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
