d53065406e06835bc5b9ff0325d98214a38f25e8
Thanh Dang Hoang Minh <InkyAshizuki@gmail.com>
2013-05-24 17:20:38 +0200

    Added logging for LeagueServiceImpl

--- src/main/java/cz/muni/fi/pv243/sportleaguesystem/service/LeagueServiceImpl.java
+++ src/main/java/cz/muni/fi/pv243/sportleaguesystem/service/LeagueServiceImpl.java
@@ -9,6 +9,8 @@ import java.util.Random;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.apache.log4j.Logger;
+
 import cz.muni.fi.pv243.sportleaguesystem.dao.interfaces.LeagueDAO;
 import cz.muni.fi.pv243.sportleaguesystem.entities.League;
 import cz.muni.fi.pv243.sportleaguesystem.entities.Match;
@@ -20,52 +22,67 @@ import cz.muni.fi.pv243.sportleaguesystem.service.interfaces.LeagueService;
 public class LeagueServiceImpl implements LeagueService {
 
 	@Inject
+	private Logger logger;
+	
+	@Inject
 	private LeagueDAO leagueDAO;
 		
 	@Override
 	public void createLeague(League league) {
         if (league == null) {
+        		logger.error("Creating a null league.");
                 throw new IllegalArgumentException("null league");
         }
         if (league.getId() != null) {
+        		logger.error("Creating a league with a set id.");
                 throw new IllegalArgumentException("league id already set");
         }
         leagueDAO.create(league);
+        logger.info("Created league. Id: " + league.getId() + " Sport: " + league.getSport());
 	}
 
 	@Override
 	public void updateLeague(League league) {
 		if (league == null) {
+			logger.error("Updating null league.");
             throw new IllegalArgumentException("null league");
 	    }
 	    if (league.getId() == null || leagueDAO.get(league.getId()) == null) {
-	            throw new IllegalArgumentException("league does not exist");
+	    	logger.error("Updating nonexistent league.");
+	        throw new IllegalArgumentException("league does not exist");
 	    }
 	    leagueDAO.update(league);
+	    logger.info("Updated league: Id: " + league.getId() + " Sport: " + league.getSport());
 	}
 
 	@Override
 	public League getById(Long id) {
 		if (id == null) {
+			logger.error("Getting league with null id.");
             throw new IllegalArgumentException("null id");
 	    }
+		logger.info("Returning league with id: " + id);
 	    return leagueDAO.get(id);
 	}
 
 	@Override
 	public void deleteLeague(League league) {
 		if (league == null) {
+			logger.error("Deleting null league.");
             throw new IllegalArgumentException("null league");
 	    }
 	    leagueDAO.delete(league);
+		logger.info("Deleted league. Id: " + league.getId() + " Sport: " + league.getSport());
 	}
 
 	@Override
 	public Map<League, Boolean> findByUser(User user, Sport sport) {
 		if (user == null) {
+			logger.error("Finding league by null user.");
             throw new IllegalArgumentException("null user");
 	    }
 		if (sport == null) {
+			logger.error("Finding league by null sport");
             throw new IllegalArgumentException("null sport");
 	    }
 		
@@ -78,40 +95,48 @@ public class LeagueServiceImpl implements LeagueService {
 				leagueMap.put(league, false);
 			}
 		}
+	    logger.info("Returning leagues found for user: " + user.getFirstName() + " " + user.getLastName());
 	    return leagueMap;
 	}
 	
 	@Override
 	public List<League> findAll(){
+		logger.info("Returning all leagues.");
 		return leagueDAO.findAll();		
 	}
 	
 	@Override
 	public List<League> findBySport(Sport sport) {
 		if (sport == null) {
+			logger.error("Finding leagues by null sport.");
             throw new IllegalArgumentException("null sport");
 	    }
-	
+		logger.info("Returning all leagues found by sport - id: " + sport.getId() + " name: " + sport.getName());
 	    return leagueDAO.findLeaguesBySport(sport);
 	}
 		
 	public void addPlayer(User user, League league){
 		
 		if (user == null ){
+			logger.error("Trying to add a null player to a league");
 			throw new IllegalArgumentException("null league");
 		}
 		if (user.getId() == null ){
+			logger.error("Trying to add a nonexistent player to a league");
 			throw new IllegalArgumentException("null league id");
 		}
 		
 		league = getById(league.getId());
-		if(! league.getPlayers().contains(user))
+		if(!league.getPlayers().contains(user)) {
 			league.getPlayers().add(user);
+		}
 		updateLeague(league);
+		logger.info("Updated league.");
 	}
 	
 	public void generateMatches(League league){
 		if (league == null) {
+			logger.error("Generating matches for a null leauge.");
             throw new IllegalArgumentException("null league");
 	    }		
 		league = getById(league.getId());
@@ -131,6 +156,7 @@ public class LeagueServiceImpl implements LeagueService {
 			match.setLeague(league);
 			league.getMatches().add(match);
 		}
+		logger.info("Updated league with new matches.");
 		updateLeague(league);		
 	}
 		
