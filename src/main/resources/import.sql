b3f549a44a1d4243ad95b0d48da4288cb996ae45
Filip Bogyai <fbogyai@redhat.com>
2013-06-01 20:29:05 +0200

    Removed secondary authetication methods, added ADMIN account when database is created

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 -- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
+insert into User (id, firstName, lastName, phone_number) values (1, 'admin', 'first', '2125551212')  
+insert into Principal (loginName, password, role, user_id) values ('admin', '21232f297a57a5a743894a0e4a801fc3', 'ADMIN', '1')  
