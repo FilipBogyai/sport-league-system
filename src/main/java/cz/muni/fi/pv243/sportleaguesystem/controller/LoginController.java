2645d940c980177514de751fa36b02c8ee15541e
Thanh Dang Hoang Minh <InkyAshizuki@gmail.com>
2013-06-07 17:26:22 +0200

    Added logging to LoginController

--- src/main/java/cz/muni/fi/pv243/sportleaguesystem/controller/LoginController.java
+++ src/main/java/cz/muni/fi/pv243/sportleaguesystem/controller/LoginController.java
@@ -8,8 +8,13 @@ import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
+
 @Model
 public class LoginController {
+	
+	@Inject
+	private Logger logger;
 	
 	@Inject
 	private FacesContext facesContext;
@@ -37,7 +42,9 @@ public class LoginController {
 		HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
 		try {
 			request.login(loginName, password);
+			logger.info("The user with loginName=" + loginName + " has logged in.");
 		} catch (ServletException e) {
+			logger.error("There was an error while logging in.");
 			return "/login?faces-redirect=true&error=true";
 		}
 		return "/index?faces-redirect=true";
@@ -46,6 +53,7 @@ public class LoginController {
 	public String logout() throws ServletException {
 		HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
 		request.logout();
+		logger.info("User " + request.getRemoteUser() + " has logged out.");
 		return "/login?faces-redirect=true";
 	}
 }
